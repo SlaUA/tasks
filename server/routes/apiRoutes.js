ec2494672616a6615873cd9219cdeb8e295bc06d
Slava Lubenets <viacheslav.lubenets@oracle.com>
2017-04-17 21:30:07 +0300

    server validation modified

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -23,43 +23,56 @@ function validateUserInput(req, res, next) {
 	req.body.password = req.sanitizeBody('password').escape().trim();
 	
 	req.checkBody({
+		
 		username: {
-			notEmpty: true,
+			notEmpty: {
+				errorMessage: 'Логин не может быть пустым'
+			},
 			isLength: {
-				options: [{ min: 5, max: 10 }],
+				options: [{min: 5,max: 10}],
 				errorMessage: 'Логин должен содерхать от 5 до 10 символов'
 			},
-			errorMessage: 'Неверный логин и/или пароль'
+			matches: {
+				options: [
+					// lowercase, uppercase, number
+					/^[a-zA-Z0-9]+$/,
+					/[a-z]/
+				],
+				errorMessage: 'Логин может содержать только латинские буквы (хотя бы одну) и цифры'
+			}
 		},
+		
 		password: {
-			notEmpty: true,
+			notEmpty: {
+				errorMessage: 'Пароль не может быть пустым'
+			},
 			isLength: {
-				options: [{ min: 6, max: 10 }],
+				options: [{min: 6,max: 10}],
 				errorMessage: 'Пароль должен содерхать от 6 до 10 символов'
 			},
 			matches: {
-				options: [/example/i],
-				errorMessage: 'example!'
-			},
-			errorMessage: 'Неверный логин и/или пароль'
+				// lowercase, uppercase, number
+				options: [
+					/^[a-zA-Z0-9]+$/
+				],
+				errorMessage: 'Пароль может содержать только латинские буквы (хотя бы одну) и цифры'
+			}
 		}
 	});
 	
 	req.getValidationResult().then(function (result) {
 		
-		console.log(result.mapped());
-		
 		if (result.isEmpty()) {
 			return next();
 		}
 		
+		console.log(result.useFirstErrorOnly().array());
+		
 		res.json({
 			code: API_CONSTANTS.ERROR_CODE,
-			message: API_CONSTANTS.ERROR_MESSAGE,
-			payload: result
+			message: result.useFirstErrorOnly().array()[0].msg
 		});
 	});
-	
 }
 
 apiRoutes
